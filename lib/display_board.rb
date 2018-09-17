41b7ca960984afd1f286711081b3ba849a83939e
Suheyl Turac <skbsturac@live.com>
2018-09-17 23:15:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,11 +7,11 @@ def display_board(board)
   
   #board = ["O","X"," "," ","X"," ","X","O"," "]
   
-  puts "#{board[0]} #{separator} #{board[1]} #{separator} #{board[2]}"
+  puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
   puts "#{lines}"
-  puts "#{board[3]} #{separator} #{board[4]} #{separator} #{board[5]}"
+  puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
   puts "#{lines}"
-  puts "#{board[6]} #{separator} #{board[7]}  #{separator} #{board[8]}"
+  puts " #{board[6]} #{separator} #{board[7]}  #{separator} #{board[8]} "
   
   #puts " X  |   |   "
   #puts "-----------"
